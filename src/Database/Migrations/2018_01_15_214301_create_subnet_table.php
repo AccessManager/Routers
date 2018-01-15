603416bf14af915a7bdb3887d16ed5b063777cd4
Rahul makhija <rahulmkhj@gmail.com>
2018-01-15 22:01:45 +0530

    created migrations to store subnet/IPs to assign static IPs on subscriptions.

---
+++ src/Database/Migrations/2018_01_15_214301_create_subnet_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubnetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('network_subnets', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('subnet');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('network_subnets');
+    }
+}
---
+++ src/Database/Migrations/2018_01_15_214501_create_subnet_ip_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubnetIpTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('network_subnet_ips', function(Blueprint $table){
+            $table->increments('id');
+            $table->unsignedInteger('subnet_id');
+            $table->unsignedInteger('address');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->timestamp('assigned_on')->nullable();
+
+            $table->foreign('subnet_id')
+                ->references('id')->on('network_subnets')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('network_subnet_ips');
+    }
+}
