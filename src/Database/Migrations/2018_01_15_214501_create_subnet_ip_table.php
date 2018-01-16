8526f754e1bdfa4be59d7bb9f997550ef594d44f
Rahul makhija <rahulmkhj@gmail.com>
2018-01-16 13:29:04 +0530

    renamed column subnet_id to network_subnet_id

--- src/Database/Migrations/2018_01_15_214501_create_subnet_ip_table.php
+++ src/Database/Migrations/2018_01_15_214501_create_subnet_ip_table.php
@@ -14,12 +14,12 @@ class CreateSubnetIpTable extends Migration
     {
         Schema::create('network_subnet_ips', function(Blueprint $table){
             $table->increments('id');
-            $table->unsignedInteger('subnet_id');
+            $table->unsignedInteger('network_subnet_id');
             $table->unsignedInteger('address');
             $table->unsignedInteger('user_id')->nullable();
             $table->timestamp('assigned_on')->nullable();
 
-            $table->foreign('subnet_id')
+            $table->foreign('network_subnet_id')
                 ->references('id')->on('network_subnets')
                 ->onDelete('cascade');
         });
