3138f0e4a4ca37a4ecc37f3d7d605b3a1b8ec6f1
Rahul makhija <rahulmkhj@gmail.com>
2018-01-16 08:55:43 +0530

    implemented getIndex & getAdd methods in SubnetController.

---
+++ src/Http/Controllers/SubnetController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AccessManager\Routers\Http\Controllers;
+
+
+use AccessManager\Base\Http\Controller\AdminBaseController;
+use AccessManager\Routers\Models\NetworkSubnet;
+
+class SubnetController extends AdminBaseController
+{
+    public function getIndex()
+    {
+        $subnets = NetworkSubnet::paginate(10);
+        return view('Routers::network.subnet-index', compact('subnets'));
+    }
+
+    public function getAdd()
+    {
+        return view('Routers::network.subnet-add');
+    }
+
+    public function postAdd()
+    {
+
+    }
+
+    public function postDelete()
+    {
+
+    }
+}
